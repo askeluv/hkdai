2ae17f73bb17cad55c0a7f3d4799c90a1801e3c2
askeluv <alexander.svanevik@gmail.com>
2019-10-05 18:36:39 +0800

    Added error messages to require statements in SafeMath.sol

--- token/contracts/SafeMath.sol
+++ token/contracts/SafeMath.sol
@@ -3,18 +3,18 @@ pragma solidity ^0.5.0;
 library SafeMath {
     function add(uint a, uint b) internal pure returns (uint c) {
         c = a + b;
-        require(c >= a); // dev: overflow
+        require(c >= a, "Overflow");
     }
     function sub(uint a, uint b) internal pure returns (uint c) {
-        require(b <= a); // dev: underflow
+        require(b <= a, "Underflow");
         c = a - b;
     }
     function mul(uint a, uint b) internal pure returns (uint c) {
         c = a * b;
-        require(a == 0 || c / a == b); // dev: overflow
+        require(a == 0 || c / a == b, "Overflow");
     }
     function div(uint a, uint b) internal pure returns (uint c) {
-        require(b > 0); // dev: divide by zero
+        require(b > 0, "Divide by zero");
         c = a / b;
     }
 }
